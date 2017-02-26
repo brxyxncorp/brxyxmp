595043fbc1f95f0fc3c5fee0c6669cd1e4dfaf80
Brayan Lopez <brxyxn.corp@live.com>
2017-02-25 21:20:57 -0600

    Fixed : -> = in enviroment/production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'http://brxyxmp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings: {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: "brxyxmp.herokuapp.com",
